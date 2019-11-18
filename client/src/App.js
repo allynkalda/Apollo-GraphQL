e54864c4fc7983119c0b953fe99f574cfb6d67f8
Allyn Alda <allyn.alda@gmail.com>
2019-11-18 20:19:33 +0100

    client ready

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="spacex"
+        style={{ width: 300, display: 'block', margin: 'auto'}} />
+    </div>
+  );
+}
+
+export default App;
